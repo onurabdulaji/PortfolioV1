5ffcb518a27749217edde9e6f40fe9f0d1db1bd6
onurabdulaji <onurabdulaji@gmail.com>
2025-04-07 15:02:40 +0200

    Hero icin Read Repository olusturuldu. Read ve Write olarak ayirdik cunku boyle daha moduler bir yapi yakalayabilriz

---
+++ Core/PortfolioV1.Domain/IRepositories/IAbstracts/IHeroRepositories/IHeroReadRepository.cs
@@ -0,0 +1,10 @@
+﻿using PortfolioV1.Domain.Entities;
+using PortfolioV1.Domain.IRepositories.IGenerics;
+
+namespace PortfolioV1.Domain.IRepositories.IAbstracts.IHeroRepositories;
+
+public interface IHeroReadRepository : IGenericReadRepository<Hero>
+{
+    Task<IList<Hero>> GetAllHeroListAsync(bool trackingChanges = false , CancellationToken cancellationToken = default);
+    Task<Hero> GetHeroByIdAsync(string id, bool trackingChanges = false, CancellationToken cancellationToken = default);
+}
