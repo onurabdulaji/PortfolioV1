0b50ba975a991b4bf3ac407d3cd1a1b6ce53c903
onurabdulaji <onurabdulaji@gmail.com>
2025-04-07 14:37:14 +0200

    Domain katmanin Generic Design Pattern interfacelerini tamamladik

---
+++ Core/PortfolioV1.Domain/IRepositories/IGenerics/IGenericReadRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Query;
+using PortfolioV1.Domain.Entities.IBase;
+using System.Linq.Expressions;
+
+namespace PortfolioV1.Domain.IRepositories.IGenerics;
+
+public interface IGenericReadRepository<T> where T : class , IBaseEntity , new()
+{
+    Task<IList<T>> GetAllAsync(Expression<Func<T, bool>>? predicate = null,
+          Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null,
+          Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+          bool enableTracking = false);
+    Task<T> GetAsync(Expression<Func<T, bool>> predicate,
+            Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null,
+            bool enableTracking = false);
+    IQueryable<T> Find(Expression<Func<T, bool>> predicate, bool enableTracking = false);
+
+    Task<int> CountAsync(Expression<Func<T, bool>>? predicate = null);
+
+    Task<T> FindAsync(Expression<Func<T, bool>> predicate);
+}
---
+++ Core/PortfolioV1.Domain/IRepositories/IGenerics/IGenericWriteRepository.cs
@@ -0,0 +1,11 @@
+﻿using PortfolioV1.Domain.Entities.IBase;
+
+namespace PortfolioV1.Domain.IRepositories.IGenerics;
+
+public interface IGenericWriteRepository<T> where T : class , IBaseEntity , new()
+{
+    Task AddAsync(T entity);
+    Task AddRangeAsync(IList<T> entities);
+    Task<T> UpdateAsync(T entity);
+    Task<T> DeleteAsync(T entity);
+}
