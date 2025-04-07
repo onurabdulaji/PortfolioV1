c594439e2ca5c1a646d290771fb95f90d3ded495
onurabdulaji <onurabdulaji@gmail.com>
2025-04-07 14:01:37 +0200

    Fluent Api islemleri olusturuldu ve Hero icin config olusturuldu

---
+++ Infrastructure/PortfolioV1.Persistence/Configurations/BaseConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PortfolioV1.Domain.Entities.IBase;
+
+namespace PortfolioV1.Persistence.Configurations;
+
+public class BaseConfiguration<T> : IEntityTypeConfiguration<T> where T : class, IBaseEntity
+{
+    public virtual void Configure(EntityTypeBuilder<T> builder)
+    {
+        builder.Property(q => q.CreatedDate)
+            .IsRequired();
+    }
+}
---
+++ Infrastructure/PortfolioV1.Persistence/Configurations/HeroConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PortfolioV1.Domain.Entities.IBase;
+
+namespace PortfolioV1.Persistence.Configurations;
+
+public class HeroConfiguration : BaseConfiguration<Hero>
+{
+    public override void Configure(EntityTypeBuilder<Hero> builder)
+    {
+        base.Configure(builder);
+    }
+}
