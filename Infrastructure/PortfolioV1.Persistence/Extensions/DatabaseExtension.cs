2288b78a6e98ec845f2ab059c646664367fb1dcc
onurabdulaji <onurabdulaji@gmail.com>
2025-04-07 14:06:55 +0200

    Database Eklentisi ve DB Connection String Eklendi

---
+++ Infrastructure/PortfolioV1.Persistence/Extensions/DatabaseExtension.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PortfolioV1.Persistence.Context.Data;
+
+namespace PortfolioV1.Persistence.Extensions;
+
+public static class DatabaseExtension
+{
+    public static void AddDatabaseExtension(this IServiceCollection services , IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("PortfolioV1DatabaseConnection"));
+        });
+    }
+}
