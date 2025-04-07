47202d961f8d3357732309b696f346293f28f21f
onurabdulaji <onurabdulaji@gmail.com>
2025-04-07 13:59:27 +0200

    DbContext eklendi

---
+++ Infrastructure/PortfolioV1.Persistence/Context/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PortfolioV1.Domain.Entities.IBase;
+
+namespace PortfolioV1.Persistence.Context.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options) { }
+
+    public AppDbContext() { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
+
+    public DbSet<Hero> Heroes { get; set; }
+}
